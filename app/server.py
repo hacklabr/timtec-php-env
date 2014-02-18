9942892df03b844d4061709c08598fd0d0824297
Bruno Gola <brunogola@gmail.com>
2014-02-18 17:09:33 -0300

    changing container name to phpUSERID instead of userid only

--- app/server.py
+++ app/server.py
@@ -16,7 +16,7 @@ def containers(request):
 
 @d("/<int:userid>/")
 def info(request, userid):
-    return dock.inspect_container(userid)
+    return dock.inspect_container("php%s" % userid)
 
 @d("/<int:userid>/url/")
 def url(request, userid):
@@ -25,42 +25,42 @@ def url(request, userid):
 @d("/<int:userid>/start/")
 def start(request, userid):
     try:
-        running = dock.inspect_container(userid)['State']['Running']
+        running = dock.inspect_container("php%s" % userid)['State']['Running']
     except docker.client.APIError:
         running = False
         dock.create_container("hacklab/precise-php-fpm-nginx", detach=True,
-                               volumes=['/var/www'], name="%s" % userid)
+                               volumes=['/var/www'], name="php%s" % userid)
     finally:
         if not running:
-            if not os.path.exists("/var/www/containers/%s" % userid):
-                os.makedirs("/var/www/containers/%s" % userid)
-            dock.start(userid, binds={'/var/www/containers/%s' % userid: '/var/www'})
-            f = open("/var/www/containers/%s/info.php" % userid, 'w')
+            if not os.path.exists("/var/www/containers/php%s" % userid):
+                os.makedirs("/var/www/containers/php%s" % userid)
+            dock.start("php%s" % userid, binds={'/var/www/containers/php%s' % userid: '/var/www'})
+            f = open("/var/www/containers/php%s/info.php" % userid, 'w')
             f.write("<? phpinfo(); ?>")
             f.close()
 
     url = "php%s.timtec.com.br" % userid
-    if not red.sdiff("frontend:%s" % userid, "dead:%s" % userid):
+    if not red.sdiff("frontend:%s" % url, "dead:%s" % url):
         red.spop("dead:%s" % url)
-        container = dock.inspect_container(userid)
+        container = dock.inspect_container("php%s" % userid)
         ip = container["NetworkSettings"]['IPAddress']
         red.sadd("frontend:%s" % url, "http://%s:80" % ip)
     return {'result': True}
 
 @d("/<int:userid>/stop/")
 def stop(request, userid):
-    dock.stop(userid)
+    dock.stop("php%s" % userid)
     return {'result': True}
 
 @d("/<int:userid>/restart/")
 def restart(request, userid):
-    dock.restart(userid)
+    dock.restart("php%s" % userid)
     return start(request, userid)
 
 @d("/<int:userid>/rm/")
 def rm(request, userid):
-    dock.stop(userid)
-    dock.remove_container(userid)
+    dock.stop("php%s" % userid)
+    dock.remove_container("php%s" % userid)
     return {'result': True}
 
 @d("/<int:userid>/documents/")
@@ -69,7 +69,7 @@ def documents(request, userid):
         return d.render_to_response("form.html")
     tgz = request.FILES['tgz']
     tf = tarfile.open(fileobj=tgz, mode="r:gz")
-    tf.extractall("/var/www/containers/%s/" % userid)
+    tf.extractall("/var/www/containers/php%s/" % userid)
     return {'result': True}
 
 if __name__ == "__main__":
